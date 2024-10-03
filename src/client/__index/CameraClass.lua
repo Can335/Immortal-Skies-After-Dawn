f2f106aef425a9948b8d133d72a42f99c9f35a78
MetaTableSetMetaTabl <canlahu1@gmail.com>
2024-10-03 13:53:56 +0200

    Push

--- src/client/__index/CameraClass.lua
+++ src/client/__index/CameraClass.lua
@@ -1,8 +1,15 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Camera = workspace.CurrentCamera
 
 local SpringUtil = require(ReplicatedStorage.Packages.spring)
 local SignalUtil = require(ReplicatedStorage.Packages.signal)
 local QueueUtil = require(ReplicatedStorage.Packages.queue)
+local UserInputService = game:GetService("UserInputService")
+
+
+local Lerp = function(a, b, t)
+	return a + (b - a) * t
+end
 
 export type CameraCS<T> = {
 
@@ -32,15 +39,29 @@ Crucial_Settings :{
 
     CameraActive: boolean,
     state : string,
-    CameraOffset: Vector3,
-    AimOffset: Vector3
+    NormalCameraOffset: Vector3,
+    AimingCameraOffset: Vector3,
+    CameraSway: boolean,
+    NormalCameraSwayDamper: number,
+    NormalCameraSwaySpeed: number,
+    AimingCameraSwayDamper: number,
+  AimingCameraSwaySpeed: number,
+  Smoothing: number
 
 },
+
+
 
 Character : Model,
 Update: (self:CameraCS<T>) -> (),
 Activate : (self:CameraCS<T>) -> (),
-Deactivate : (self:CameraCS<T>) -> ()
+Deactivate : (self:CameraCS<T>) -> (),
+AimingSpring : SpringUtil.Spring,
+NormalSpring : SpringUtil.Spring,
+Start : (self: CameraCS<T>) -> (),
+TargetRotation: Vector2,
+LerpedRotation : any,
+currentOffset :any
 
 }
 
@@ -52,6 +73,8 @@ function CameraClass.New(Character:Model) : CameraCS<any>
     
 local self : CameraCS<any> = setmetatable({}, CameraClass) :: any
 
+
+
 self.Character = Character
 
 self.Settings = {
@@ -61,6 +84,7 @@ CameraBobbingSpeed =  0,
 CameraBobbingStrength = {x = 0, y = 0, z = 0},
 CameraActive = true
 
+
 }
 
 self.States = {"Aiming", "Normal", "Cutscene","NaN"}
@@ -69,10 +93,24 @@ self.Crucial_Settings = {
 
 CameraActive =  false,
 state = "NaN",
-CameraOffset = Vector3.new(2, 1, 8),
-AimOffset = Vector3.new(1.5, .5, 5)
+NormalCameraOffset = Vector3.new(2, 1, 8),
+AimingCameraOffset = Vector3.new(1.5, .5, 5),
+CameraSway = true,
+NormalCameraSwayDamper = .5,
+NormalCameraSwaySpeed = 2,
+AimingCameraSwayDamper = 1,
+AimingCameraSwaySpeed = 100,
+Smoothing =  15
 
 }
+
+self.NormalSpring = SpringUtil.new(self.Crucial_Settings.NormalCameraSwayDamper,self.Crucial_Settings.NormalCameraSwaySpeed,Vector2.new())
+self.AimingSpring = SpringUtil.new(self.Crucial_Settings.AimingCameraSwayDamper,self.Crucial_Settings.AimingCameraSwaySpeed,Vector2.new())
+
+self.TargetRotation = Vector2.new(0, 0)
+self.LerpedRotation = self.TargetRotation
+self.currentOffset = "NAN"
+
 
 return self :: CameraCS<any>
 end
@@ -99,12 +137,70 @@ function CameraClass:Cutscene()
     
 end
 
-function CameraClass:Update()
+
+
+function CameraClass:Update(deltaTime)
+
+    if self.Crucial_Settings.CameraActive == false then
+        return
+    end
     
+
+
+    local MouseDelta = -UserInputService:GetMouseDelta() / self.Crucial_Settings.Smoothing
+    self.TargetRotation = Vector2.new(
+				math.clamp(
+                    self.TargetRotation.X + MouseDelta.Y, -75, 75),
+                    self.TargetRotation.Y + MouseDelta.X
+			)
+
+            self.LerpedRotation = Lerp(  self.LerpedRotation , self.TargetRotation, deltaTime * self.Crucial_Settings.Smoothing)
+			 
+
+			local offsetCFrame = CFrame.new(0, self.currentOffset.Y, 0)
+			local rotation = CFrame.fromEulerAnglesYXZ(math.rad(  self.LerpedRotation .X), math.rad(  self.LerpedRotation .Y), 0)
+			local targetCFrame = CFrame.new(self.Character.HumanoidRootPart.Position) * offsetCFrame * rotation
+			Camera.Focus = targetCFrame
+
+local Sway_Temp 
+
+            if self.Crucial_Settings.state == "Aiming" then
+                
+self.AimingSpring:Set(Vector2.new(MouseDelta.X, MouseDelta.Y) * 0.04)
+self.AimingSpring:Step(deltaTime)
+Sway_Temp = self.AimingSpring.Position
+                else
+
+                    self.NormalSpring:Set(Vector2.new(MouseDelta.X, MouseDelta.Y) * 0.04)
+                    self.NormalSpring:Step(deltaTime)
+Sway_Temp = self.NormalSpring.Position
+            end
+
+
+local Sway_Temp_CF = CFrame.Angles(0, math.rad(Sway_Temp.X), math.rad(Sway_Temp.Y))
+
+Camera.CFrame = targetCFrame * CFrame.new(self.currentOffset) * Sway_Temp_CF
+
 end
 
 function CameraClass:Shake()
     
+end
+
+function CameraClass:Start()
+
+    self:Activate()
+    self:ChangeState("Normal")
+    task.wait()
+    self.currentOffset = self.Crucial_Settings[self.Crucial_Settings.state .. "CameraOffset"]
+    task.wait()
+    
+game:GetService("RunService").RenderStepped:Connect(function(deltaTime)
+    
+self:Update(deltaTime)
+
+end)
+
 end
 
 return CameraClass
--- src/client/__index/MovementClass.lua
+++ src/client/__index/MovementClass.lua
@@ -14,10 +14,14 @@ New: () -> MovementCS<T>,
 Add: (self: MovementCS<T>,KeyCode : Enum.KeyCode) -> (boolean),
 Remove : (self: MovementCS<T>,KeyCode : Enum.KeyCode) -> (boolean),
 Start : (self: MovementCS<T>,CharacterCL : ControllerManager) -> (),
-
+Character : Model,
 KeysAccepting : {Enum.KeyCode},
 
+CurrentLookVector : Vector3,
+CurrentRightVector: Vector3,
+
 CurrentTypeCam : "Normal" | "Directional" ,
+MoveVector: Vector3,
 
 QueueAssignedVector : {
 
@@ -37,7 +41,7 @@ QueueSettingsDirectionFacing : {
 
 },
 
-InitState: (self: MovementCS,CharacterCL: ControllerManager) -> (boolean)
+InitState: (self: MovementCS,CharacterCL: ControllerManager,deltatime) -> (boolean)
 
 }
 
@@ -45,16 +49,20 @@ local MovementClass = {} :: MovementCS<any>
 
 MovementClass.__index = MovementClass
 
-function MovementClass.New(): MovementCS<any>
+function MovementClass.New(Character: Model): MovementCS<any>
     local self : MovementCS<any> = setmetatable({}, MovementClass) :: any
+self.CurrentLookVector =  Vector3.new(Camera.CFrame.LookVector.X, 0, Camera.CFrame.LookVector.Z).Unit
+self.CurrentRightVector =  Vector3.new(Camera.CFrame.RightVector.X, 0, Camera.CFrame.RightVector.Z).Unit
+self.MoveVector = Vector3.new(0,0,0)
+self.Character = Character
 
     self.CurrentTypeCam = "Normal"
 
     self.QueueSettingsDirection = {
 
 [Enum.KeyCode.W] = {["Normal"] = {[1] = 1, [2] = 1,[3] = 0, [4] = 0}, ["Directional"] = {[1] = 1, [2] = 1, [3] = 0, [4] = 0}},
-[Enum.KeyCode.S] = {["Normal"] = {[1] = 1, [2] = 1,[3] = 0, [4] = 0}, ["Directional"] = {[1] = 1, [2] = 1, [3] = 0, [4] = 0}},
-[Enum.KeyCode.A] = {["Normal"] = {[1] = 1, [2] = 1,[3] = 0, [4] = 0}, ["Directional"] = {[1] = 1, [2] = 1, [3] = 0, [4] = 0}},
+[Enum.KeyCode.S] = {["Normal"] = {[1] = -1, [2] = -1,[3] = 0, [4] = 0}, ["Directional"] = {[1] = 1, [2] = 1, [3] = 0, [4] = 0}},
+[Enum.KeyCode.A] = {["Normal"] = {[1] = -1, [2] = -1,[3] = 0, [4] = 0}, ["Directional"] = {[1] = 1, [2] = 1, [3] = 0, [4] = 0}},
 [Enum.KeyCode.D] = {["Normal"] = {[1] = 1, [2] = 1,[3] = 0, [4] = 0}, ["Directional"] = {[1] = 1, [2] = 1, [3] = 0, [4] = 0}}
 
     }
@@ -68,7 +76,16 @@ function MovementClass.New(): MovementCS<any>
         
             }
 
-self.KeysAccepting = {Enum.KeyCode.W, Enum.KeyCode.D, Enum.KeyCode.A, Enum.KeyCode.D}
+self.KeysAccepting = {Enum.KeyCode.W, Enum.KeyCode.D, Enum.KeyCode.A, Enum.KeyCode.S}
+
+self.QueueAssignedVector = {
+
+[Enum.KeyCode.A] = "X",
+[Enum.KeyCode.D] = "X",
+[Enum.KeyCode.W] = "Z",
+[Enum.KeyCode.S] = "Z",
+
+}
 
 self._Queue = QueueUtil.new({}) :: QueueUtil.Queue<any>
 
@@ -79,24 +96,42 @@ end
 function MovementClass:Add(KeyCode : Enum.KeyCode) : boolean
     
 self._Queue:pushBack(KeyCode)
+print(self._Queue)
 return true
 end
 
 function MovementClass:Remove(KeyCode: Enum.KeyCode) : boolean
     self._Queue:remove(table.find(self._Queue.list, KeyCode))
+    print(self._Queue)
     return true
 end
 
-function MovementClass:InitState(CharacterCL : ControllerManager)
+function MovementClass:InitState(CharacterCL : ControllerManager, deltaTime)
     
 local checkedvector = false
-local MoveVector = Vector3.new(0,0,0)
+local MoveVector = {Z = 0, X = 0, Y = 0}
 local LookVector = Vector3.new(Camera.CFrame.LookVector.X, 0, Camera.CFrame.LookVector.Z).Unit
 local RightVector = Vector3.new(Camera.CFrame.RightVector.X, 0, Camera.CFrame.RightVector.Z).Unit
+local smoothingFactor = 10  -- Adjust this value to control the smoothness
+
+
+if not self.currentLookVector then
+    self.currentLookVector = LookVector
+    self.currentRightVector = RightVector
+    else
+        self.currentLookVector = self.currentLookVector:Lerp(LookVector, (deltaTime * smoothingFactor))
+        self.currentRightVector = self.currentRightVector:Lerp(RightVector, (deltaTime * smoothingFactor))
+        
+end
+
+
+
 
 if self._Queue.list[1] == nil then
+    self.MoveVector = Vector3.new(0,0,0)
+   -- CharacterCL.FacingDirection =  self.MoveVector.X * RightVector *  self.MoveVector.Z * self.currentLookVector
+    CharacterCL.MovingDirection =  self.MoveVector.X * RightVector + self.MoveVector.Z * self.currentLookVector
     
-    CharacterCL.MovingDirection = MoveVector * RightVector * LookVector
 return
 end
 
@@ -105,19 +140,27 @@ if self._Queue.list[2] == nil then
     
 if self.QueueAssignedVector[self._Queue.list[1]] == "X" then
 
-MoveVector.X = self._Queue.list[self.CurrentTypeCam[self._Queue.list[1]][1]]
+MoveVector.X = self.QueueSettingsDirection[self._Queue.list[1]][self.CurrentTypeCam][1]
 checkedvector = true
-else
+self.MoveVector = Vector3.new(MoveVector.X,0,MoveVector.Z)
+CharacterCL.MovingDirection =   self.MoveVector.X * RightVector +  self.MoveVector.Z * self.currentLookVector
+--CharacterCL.FacingDirection =  self.MoveVector.X * RightVector *  self.MoveVector.Z * self.currentLookVector
+return
+elseif self.QueueAssignedVector[self._Queue.list[1]] == "Z" then
 
-    MoveVector.Y = self._Queue.list[self.CurrentTypeCam[self._Queue.list[1]][1]]
+    MoveVector.Z = self.QueueSettingsDirection[self._Queue.list[1]][self.CurrentTypeCam][1]
     checkedvector = true
+    self.MoveVector = Vector3.new(MoveVector.X,0,MoveVector.Z)
+    CharacterCL.MovingDirection =  self.MoveVector.X * RightVector +  self.MoveVector.Z * self.currentLookVector
+
+    return
 end
 
 end
 
 if self.QueueAssignedVector[self._Queue.list[1]] == "X" and  self.QueueAssignedVector[self._Queue.list[2]] == "X" and checkedvector == false then
 
-MoveVector.X = self.QueueSettingsDirection[self.CurrentTypeCam[self._Queue.list[1]][1]]
+MoveVector.X = self.QueueSettingsDirection[self._Queue.list[1]][self.CurrentTypeCam][1]
 
 
 
@@ -127,33 +170,35 @@ if self._Queue.list[3] ~= nil then
 end
 elseif self.QueueAssignedVector[self._Queue.list[1]] == "Z" and  self.QueueAssignedVector[self._Queue.list[2]] == "Z" then
 
-    MoveVector.Z = self.QueueSettingsDirection[self.CurrentTypeCam[self._Queue.list[1]][1]]
+    MoveVector.Z = self.QueueSettingsDirection[self._Queue.list[1]][self.CurrentTypeCam][1]
 
     
     if self._Queue.list[3] ~= nil then
         
-        MoveVector.X = self.QueueSettingsDirection[self.CurrentTypeCam[self._Queue.list[3]][2]]
+        MoveVector.X = self.QueueSettingsDirection[self._Queue.list[3]][self.CurrentTypeCam][2]
 
     end
-    elseif self.QueueAssignedVector[self._Queue.list[1]] == "Z" and  self.QueueAssignedVector[self._Queue.list[2]] == "X" then
+    elseif self.QueueAssignedVector[self._Queue.list[1]] == "Z" and  self.QueueAssignedVector[self._Queue.list[2]] == "X"  then
 
-        MoveVector.X = self.QueueSettingsDirection[self.CurrentTypeCam[self._Queue.list[2]][2]]
+        MoveVector.X = self.QueueSettingsDirection[self._Queue.list[2]][self.CurrentTypeCam][2]
 
-        MoveVector.Z = self.QueueSettingsDirection[self.CurrentTypeCam[self._Queue.list[1]][1]]
+        MoveVector.Z = self.QueueSettingsDirection[self._Queue.list[1]][self.CurrentTypeCam][1]
 
-else
+elseif  self.QueueAssignedVector[self._Queue.list[1]] == "X" and  self.QueueAssignedVector[self._Queue.list[2]] == "Z" then
 
-    MoveVector.Z = self.QueueSettingsDirection[self.CurrentTypeCam[self._Queue.list[2]][2]]
+    MoveVector.Z = self.QueueSettingsDirection[self._Queue.list[2]][self.CurrentTypeCam][2]
 
-    MoveVector.X = self.QueueSettingsDirection[self.CurrentTypeCam[self._Queue.list[1]][1]]
+    MoveVector.X = self.QueueSettingsDirection[self._Queue.list[1]][self.CurrentTypeCam][1]
 
     end    
 
+print("test")
 
-task.wait()
+self.MoveVector = Vector3.new(MoveVector.X,0,MoveVector.Z)
 
-CharacterCL.MovingDirection = MoveVector * RightVector * LookVector
-
+CharacterCL.MovingDirection =   self.MoveVector.X * RightVector +  self.MoveVector.Z * self.currentLookVector
+--CharacterCL.FacingDirection =  self.MoveVector.X * RightVector *  self.MoveVector.Z * self.currentLookVector
+return
 
 end    
 
@@ -161,6 +206,17 @@ end
 
 function MovementClass:Start(CharacterCL: ControllerManager)
     
+self.Character.Name = game.Players.LocalPlayer.Name
+
+game.Players.LocalPlayer.Character:Destroy()
+
+game.Players.LocalPlayer.Character = self.Character
+
+game:GetService("RunService").RenderStepped:Connect(function(deltaTime)
+    self:InitState(CharacterCL, deltaTime)
+end)
+
+
 UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
     
     if table.find(self.KeysAccepting,input.KeyCode) == nil then
@@ -172,7 +228,7 @@ UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
 
     self:Add(input.KeyCode)
     task.wait()
-    self:InitState(CharacterCL)
+
 
 end)
 
@@ -186,7 +242,8 @@ UserInputService.InputEnded:Connect(function(input, gameProcessedEvent)
     task.wait()
     self:Remove(input.KeyCode)
     task.wait()
-    self:InitState(CharacterCL)
+
+    
 end)
 
 end
